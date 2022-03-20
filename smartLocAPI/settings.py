edff771a161490d47de66779247dd392af03dd96
Katerina <katerina-snow@mail.ru>
2022-03-20 12:46:31 +0300

    add settings_local file

--- smartLocAPI/settings.py
+++ smartLocAPI/settings.py
@@ -70,16 +70,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'smartLocAPI.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -105,19 +95,22 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
-
 USE_I18N = True
 
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .settings_local import *
+except ImportError:
+    raise Exception(
+        'Please create settings_local.py file. '
+        'You will find sample on '
+        '"project/settings_local.py.sample"'
+    )
+
